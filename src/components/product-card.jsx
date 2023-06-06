efc82abf31108a9c8335e3a5d51ccec7ab0c8113
Nyshawn Burton <33571460+NBurton830@users.noreply.github.com>
2023-06-06 14:30:27 -0400

    Remove Demo Content from Index, Fix Product Card Images

--- src/components/product-card.jsx
+++ src/components/product-card.jsx
@@ -12,7 +12,9 @@ import {
   productPrice,
 } from "./product-card.module.css"
 
+// The ProductCard component receives a 'product' object and an 'eager' flag as props
 export function ProductCard({ product, eager }) {
+  // Extract the necessary properties from the 'product' object
   const {
     title,
     priceRangeV2,
@@ -22,17 +24,28 @@ export function ProductCard({ product, eager }) {
     storefrontImages,
   } = product
 
+  // Format the price to display
   const price = formatPrice(
     priceRangeV2.minVariantPrice.currencyCode,
     priceRangeV2.minVariantPrice.amount
   )
 
+  // Set default values for the image dimensions
   const defaultImageHeight = 200
   const defaultImageWidth = 200
+
+  // Initialize an empty object to store the storefront image data
   let storefrontImageData = {}
+
+  // Check if there are storefront images available
   if (storefrontImages) {
-    const storefrontImage = storefrontImages.edges[0].node
+    // Get the first storefront image (if available) using optional chaining
+    let storefrontImage = null;
+    if (storefrontImages && storefrontImages.edges && storefrontImages.edges.length > 0) {
+      storefrontImage = storefrontImages.edges[0].node;
+    }
     try {
+      // Generate the Shopify image URL using the 'getShopifyImage' function
       storefrontImageData = getShopifyImage({
         image: storefrontImage,
         layout: "fixed",
@@ -44,27 +57,29 @@ export function ProductCard({ product, eager }) {
     }
   }
 
+  // Check if the product has an image (either firstImage or storefrontImageData)
   const hasImage = firstImage || Object.getOwnPropertyNames(storefrontImageData || {}).length
 
+  // Render the ProductCard component
   return (
     <Link
       className={productCardStyle}
       to={slug}
       aria-label={`View ${title} product page`}
     >
-      {hasImage
-        ? (
-          <div className={productImageStyle} data-name="product-image-box">
-            <GatsbyImage
-              alt={firstImage?.altText ?? title}
-              image={firstImage?.gatsbyImageData ?? storefrontImageData}
-              loading={eager ? "eager" : "lazy"}
-            />
-          </div>
-        ) : (
-          <div style={{ height: defaultImageHeight, width: defaultImageWidth }} />
-        )
-      }
+      {hasImage ? (
+        // Render the product image if available
+        <div className={productImageStyle} data-name="product-image-box">
+          <GatsbyImage
+            alt={firstImage?.altText ?? title}
+            image={firstImage?.gatsbyImageData ?? storefrontImageData}
+            loading={eager ? "eager" : "lazy"}
+          />
+        </div>
+      ) : (
+        // Render a placeholder div if no image is available
+        <div style={{ height: defaultImageHeight, width: defaultImageWidth }} />
+      )}
       <div className={productDetailsStyle}>
         <div className={productVendorStyle}>{vendor}</div>
         <h2 as="h2" className={productHeadingStyle}>
@@ -76,6 +91,7 @@ export function ProductCard({ product, eager }) {
   )
 }
 
+// GraphQL query fragment to fetch the necessary data for the ProductCard component
 export const query = graphql`
   fragment ProductCard on ShopifyProduct {
     id
